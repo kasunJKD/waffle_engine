ccc74426c7b37b8a24874f78f8f52eab3997db67
kasunJKD <kasun.ravihara@gmail.com>
2024-04-14 12:38:38 +0530

    cube movement added

--- src/main.cpp
+++ src/main.cpp
@@ -34,22 +34,45 @@ GLuint compile_shader(GLenum type, GLsizei, const char **);
 GLuint program_check(GLuint);
 const char * read_file(const char *);
 
+// camera
+glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f, 0.0f);
+
+bool firstMouse = true;
+float yaw   = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch =  0.0f;
+float lastX =  800.0f / 2.0;
+float lastY =  600.0 / 2.0;
+float fov   =  45.0f;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
 int main (int argc, char* argv[])
 {
 
   SDL_Init(SDL_INIT_EVERYTHING);
+
+  float lastFrame = static_cast<float>(SDL_GetTicks()) / 1000.0f;
+
+   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
   Uint32 WindowFlags = SDL_WINDOW_HIDDEN|SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE;
-  SDL_Window *Window = SDL_CreateWindow("", 0, 0, 0, 0, WindowFlags);
+  SDL_Window *Window = SDL_CreateWindow("", 0, 0, 1000, 1000, WindowFlags);
   SDL_GLContext Context = SDL_GL_CreateContext(Window);
   SDL_GL_MakeCurrent(Window, Context);
+  
+
 
    SDL_SetWindowSize(Window, WinWidth, WinWidth);
    SDL_SetWindowPosition(Window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
    SDL_SetWindowTitle(Window, "engine");
    SDL_ShowWindow(Window);
-
-   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
 
    gladLoadGLLoader(SDL_GL_GetProcAddress);
 
@@ -178,36 +201,95 @@ float vertices[] = {
   b32 FullScreen = 0;
   while (Running)
   {
+   float currentFrame = static_cast<float>(SDL_GetTicks()) / 1000.0f;
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+    
+    std::cout << "Camera Position: (" 
+              << cameraPos.x << ", " 
+              << cameraPos.y << ", " 
+              << cameraPos.z << ")" << std::endl;
+    
     SDL_Event Event;
-    while (SDL_PollEvent(&Event))
+     while (SDL_PollEvent(&Event))
     {
-      if (Event.type == SDL_KEYDOWN)
-      {
-        switch (Event.key.keysym.sym)
+        if(Event.type == SDL_QUIT)
         {
-          case SDLK_ESCAPE:
-            Running = 0;
-            break;
-          case 'f':
-            FullScreen = !FullScreen;
-            if (FullScreen)
-            {
-              SDL_SetWindowFullscreen(Window, WindowFlags | SDL_WINDOW_FULLSCREEN_DESKTOP);
-            }
-            else
-            {
-              SDL_SetWindowFullscreen(Window, WindowFlags);
-            }
-            break;
-          default:
-            break;
+                Running = false;
         }
-	
-      }
-      else if (Event.type == SDL_QUIT)
+        else if (Event.type == SDL_KEYDOWN)
+        {
+                std::cout << "Key press detected: " << SDL_GetKeyName(Event.key.keysym.sym) << std::endl;
+                float cameraSpeed = static_cast<float>(5.0 * deltaTime);
+                switch (Event.key.keysym.sym)
+                {                    
+                    case SDLK_ESCAPE:
+                        Running = false;
+                        break;
+                    case SDLK_f:
+                        FullScreen = !FullScreen;
+                        SDL_SetWindowFullscreen(Window, FullScreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0);
+                        break;
+                    case SDLK_w:
+                        cameraPos += cameraSpeed * cameraFront;
+                        break;
+                    case SDLK_s:
+                        cameraPos -= cameraSpeed * cameraFront;
+                        break;
+                    case SDLK_a:
+                        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+                        break;
+                    case SDLK_d:
+                        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+                        break;
+                }
+            }
+      else if(Event.type == SDL_MOUSEMOTION)
       {
-        Running = 0;
-      }
+                if (SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_LEFT)) // Check if left button is pressed
+                {
+                    float xpos = static_cast<float>(Event.motion.x);
+                    float ypos = static_cast<float>(Event.motion.y);
+
+                    if (firstMouse) {
+                        lastX = xpos;
+                        lastY = ypos;
+                        firstMouse = false;
+                    }
+
+                    float xoffset = xpos - lastX;
+                    float yoffset = lastY - ypos;
+                    lastX = xpos;
+                    lastY = ypos;
+
+                    float sensitivity = 0.1f;
+                    xoffset *= sensitivity;
+                    yoffset *= sensitivity;
+
+                    yaw += xoffset;
+                    pitch -= yoffset;
+
+                    if (pitch > 89.0f)
+                        pitch = 89.0f;
+                    if (pitch < -89.0f)
+                        pitch = -89.0f;
+
+                    glm::vec3 front;
+                    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+                    front.y = sin(glm::radians(pitch));
+                    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+                    cameraFront = glm::normalize(front);
+                }
+          }
+            else if (Event.type == SDL_MOUSEWHEEL)
+        {
+                fov -= static_cast<float>(Event.wheel.y);
+                if (fov < 1.0f)
+                    fov = 1.0f;
+                if (fov > 45.0f)
+                    fov = 45.0f;
+                break;
+        }
     }
 
     glViewport(0, 0, WinWidth, WinHeight);
@@ -218,17 +300,16 @@ float vertices[] = {
 
     // create transformations
     glm::mat4 model         = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-    glm::mat4 view          = glm::mat4(1.0f);
-    glm::mat4 projection    = glm::mat4(1.0f);
     model = glm::rotate(model, (float)SDL_GetTicks() / 1000, glm::vec3(0.5f, 1.0f, 0.0f));
-    view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-    projection = glm::perspective(glm::radians(45.0f), (float)WinWidth/ (float)WinHeight, 0.1f, 100.0f);
+    
+    glm::mat4 view  = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+    glm::mat4 projection = glm::perspective(glm::radians(fov), (float)WinWidth/ (float)WinHeight, 0.1f, 100.0f);
     // retrieve the matrix uniform locations
     unsigned int modelLoc = glGetUniformLocation(shading_program, "model");
     unsigned int viewLoc  = glGetUniformLocation(shading_program, "view");
     // pass them to the shaders (3 different ways)
     glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
     // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
     glUniformMatrix4fv(glGetUniformLocation(shading_program, "projection"), 1, GL_FALSE, &projection[0][0]);
 
