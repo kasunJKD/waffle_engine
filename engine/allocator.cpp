1919397934547dc7bbb48f0eb50006e4ef6dee54
kasunJKD <kasun.ravihara@gmail.com>
2025-03-27 00:47:53 +0530

    allocators added

--- engine/allocator.cpp
+++ engine/allocator.cpp
@@ -1,6 +1,8 @@
 #include "allocator.h"
 #include "debug.h"
+#include <cassert>
 #include <cstddef>  // For std::max_align_t
+#include <cstdint>
 #include <cstdlib>  // For malloc, free
 #include <cstdio>   // For fprintf, printf
 #include <cstring>  // For memset
@@ -114,4 +116,149 @@ void arena_print_stats(const MemoryArena* arena) {
 }
 
 } // namespace MEM
+
+//reference
+//https://www.gingerbill.org/article/2019/02/16/memory-allocation-strategies-004/#fn:1
+bool power_of_two(uintptr_t value)
+{
+    return (value & (value-1)) == 0;
+}
+
+size_t align_forward_uintptr(size_t ptr, size_t align) {
+    size_t a, p, modulo;
+
+    DEBUG_ASSERT(power_of_two((uintptr_t)align), "power of two");
+
+    a = align;
+    p = ptr;
+    modulo = p & (a-1);
+    if (modulo != 0) {
+        p+=a-modulo;
+    }
+
+    return p;
+}
+
+size_t align_forward_size(size_t ptr, size_t align) {
+	size_t a, p, modulo;
+
+	assert(power_of_two((uintptr_t)align));
+
+	a = align;
+	p = ptr;
+	modulo = p & (a-1);
+	if (modulo != 0) {
+		p += a - modulo;
+	}
+	return p;
+}
+
+namespace Pool_Allocator {
+	void pool_init(Pool *p, void *back_buffer, size_t back_buffer_length,
+		size_t chunk_size, size_t chunk_alignment)
+        {
+            //allign back buffer
+            uintptr_t initial_start = (uintptr_t)back_buffer;
+            uintptr_t start = align_forward_uintptr(initial_start, (uintptr_t)chunk_alignment);
+            back_buffer_length -= (size_t)(start - initial_start);
+
+            //align chunk size
+            chunk_size = align_forward_size(chunk_size, chunk_alignment);
+
+            DEBUG_ASSERT(chunk_size >= sizeof(Pool_Free_Node), "Chunk size is too small");
+            DEBUG_ASSERT(back_buffer_length >= chunk_size, "Back buffer length is smaller than the chunk size");
+
+            p->buff = (unsigned char *)back_buffer;
+            p->buff_len = back_buffer_length;
+            p->chunk_size = chunk_size;
+            p->head = NULL;
+
+            pool_free_all(p);
+        }
+
+        void *pool_alloc(Pool *p) {
+            Pool_Free_Node *node = p->head;
+            if(node == NULL)
+            {
+                DEBUG_ASSERT(0, "Pool allocator has no free memory");
+                return nullptr;
+            }
+            
+            p->head = p->head->next;
+            return memset(node, 0, p->chunk_size);
+        }
+
+        void pool_free(Pool *p, void *ptr) {
+            Pool_Free_Node *node;
+
+            void *start = p->buff;
+            void *end = &p->buff[p->buff_len];
+
+            if(ptr == nullptr)
+            {
+                return;
+            }
+
+            if(!(start <= ptr && ptr < end)) {
+                DEBUG_ASSERT(0, "Memory is out of bounds of the buffer in this pool");
+                return;
+            }
+
+            node = (Pool_Free_Node *)ptr;
+            node->next = p->head;
+            p->head = node;
+        }
+
+        void pool_free_all(Pool *p) {
+                size_t chunk_count = p->buff_len / p->chunk_size;
+                size_t i;
+
+                // Set all chunks to be free
+                for (i = 0; i < chunk_count; i++) {
+                        void *ptr = &p->buff[i * p->chunk_size];
+                        Pool_Free_Node *node = (Pool_Free_Node *)ptr;
+                        // Push free node onto thte free list
+                        node->next = p->head;
+                        p->head = node;
+                }
+        }
+}
+
+namespace Temp_Allocator {
+        void temp_arena_init(TempArena *a, void *backing_buffer, size_t backing_buffer_length) {
+                a->buff = (unsigned char *)backing_buffer;
+                a->buffer_length = backing_buffer_length;
+                a->curr_offset = 0;
+                a->prev_offset = 0;
+        }
+
+        void *temp_arena_alloc_align(TempArena *a, size_t size, size_t align) {
+                // Align 'curr_offset' forward to the specified alignment
+                uintptr_t curr_ptr = (uintptr_t)a->buff + (uintptr_t)a->curr_offset;
+                uintptr_t offset = align_forward_uintptr(curr_ptr, align);
+                offset -= (uintptr_t)a->buff; // Change to relative offset
+
+                // Check to see if the backing memory has space left
+                if (offset+size <= a->buffer_length) {
+                        void *ptr = &a->buff[offset];
+                        a->prev_offset = offset;
+                        a->curr_offset = offset+size;
+
+                        // Zero new memory by default
+                        memset(ptr, 0, size);
+                        return ptr;
+                }
+                // Return NULL if the arena is out of memory (or handle differently)
+                return NULL;
+        }
+
+        void temp_arena_free_all(TempArena*a) {
+                a->curr_offset = 0;
+                a->prev_offset = 0;
+        }
+
+        void *temp_arena_alloc(TempArena *a, size_t size) {
+                return temp_arena_alloc_align(a, size, 2*sizeof(void *));
+        }
+}
 
--- engine/allocator.h
+++ engine/allocator.h
@@ -2,10 +2,17 @@
 #define ALLOCATOR_H
 
 #include <cstddef>
+#include <cstring>
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+
+bool power_of_two(uintptr_t value);
+size_t align_forward_uintptr(size_t ptr, size_t align);
+size_t align_forward_size(size_t ptr, size_t align);
+
 namespace MEM {
+
 struct MemoryArena
 {
 	void* base;
@@ -21,4 +28,44 @@ void arena_reset(MemoryArena* arena);
 void arena_print_stats(const MemoryArena* arena);
 }
 
+//used for persistant data otherwise there can be fragmentation
+namespace Pool_Allocator {
+
+	struct Pool_Free_Node{
+		Pool_Free_Node *next;
+	};
+
+	struct Pool  {
+		unsigned char* buff;
+		size_t buff_len;
+		size_t chunk_size;
+
+		Pool_Free_Node *head;
+	};
+
+	void pool_init(Pool *p, void *back_buffer, size_t back_buffer_length,
+		size_t chunk_size, size_t chunk_alignment);
+	void pool_free_all(Pool *p);
+        void *pool_alloc(Pool *p);
+        void pool_free(Pool *p, void *ptr);
+
+}
+
+//used for bump allocator (linear/arena allocation)
+namespace Temp_Allocator {
+
+	struct TempArena  {
+		unsigned char *buff;
+		size_t buffer_length;
+		size_t curr_offset;
+		size_t prev_offset;
+	};
+
+	void *temp_alloc(TempArena *t, size_t size);
+
+}
+
 #endif
+
+
+
